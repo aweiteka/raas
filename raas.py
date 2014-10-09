cb8556e443bf50070af0c02ab03581af4a35bba2
aweiteka <aweiteka@redhat.com>
2014-10-08 21:28:09 -0400

    grab correct path for crane metadata file

--- raas.py
+++ raas.py
@@ -5,6 +5,7 @@ import boto
 from boto.s3.key import Key
 import tarfile
 import tempfile
+import glob
 
 class PulpTar(object):
     def __init__(self, tarfile):
@@ -13,11 +14,16 @@ class PulpTar(object):
 
     @property
     def crane_metadata_file(self):
-        return self.tar_tempdir + "/crane-metadata.json"
+        json_files = glob.glob(self.tar_tempdir + "/*.json")
+        if len(json_files) == 1:
+            return json_files[0]
+        else:
+            print "More than one metadata file found"
+            exit(1)
 
     @property
-    def docker_image_tarfile(self):
-        return self.tarfile
+    def docker_images_dir(self):
+        return self.tar_tempdir + "/web"
 
     def extract_tar(self):
         tar = tarfile.open(self.tarfile)
@@ -27,21 +33,21 @@ class PulpTar(object):
         tar.close()
 
 class AwsS3(object):
-    def __init__(self, bucket, app, image_tar):
+    def __init__(self, bucket, app, images_dir):
         self.bucket = bucket
         self.app = app
-        self.image_tar = image_tar
+        self.images_dir = images_dir
 
     @property
     def key(self):
-        return self.app + "/" + self.image_tar
+        return self.app + "/" + self.images_dir
 
     def upload_tar(self):
         s3 = boto.connect_s3()
         bucket = s3.create_bucket(self.bucket)
         k = Key(bucket)
         k.key = self.key
-        k.set_contents_from_filename(self.image_tar)
+        k.set_contents_from_filename(self.images_dir)
         k.set_acl('public-read')
         for key in bucket.list():
             print key.name
@@ -65,7 +71,7 @@ def main():
 
     pulp = PulpTar(args.tarfile)
     pulp.extract_tar()
-    #s3 = AwsS3(args.bucket_name, args.app_name, pulp.docker_image_tarfile)
+    #s3 = AwsS3(args.bucket_name, args.app_name, pulp.docker_images_dir)
     #s3.upload_tar()
 
 
